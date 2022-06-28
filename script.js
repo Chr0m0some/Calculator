8ee9651b53eed964be041b8c2022d3bdf640521c
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-28 09:03:34 -0500

    Add all basic operations

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Operations */
+function add(operator, operand) {return operator + operand}; 
+function subtract(operator, operand) {return operator - operand};
+function multiply(operator, operand) {return operator * operand};
+function divide(operator, operand) {return (operand === 0) ? alert('Woah buddy...') : operator / operand};
+

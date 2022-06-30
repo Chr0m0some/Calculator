ca9fe0b58729b6a6d6587059df7adce6f773b2c9
Chr0m0some <78382311+Chr0m0some@users.noreply.github.com>
2022-06-30 09:14:07 -0500

    Progress on readyOperation function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let prev_num;
-let current_num;
+let current_num = '0';
 let inputs = [];
 let operator_input;
 /* Operations */
@@ -30,29 +30,53 @@ function operate(operator, num1, num2) {
 
 /* 
 
-    Default number of 0 is on the display and set up as an operand1
+    Default number of 0 is on the display and set up as the current number on display
     IF the user were to click a number:
-        the number would be set up as operand1 instead
+        the number would be set up as the current number instead of 0
+        IF the current number is still staged: 
+            Add on to the number as a string
     IF the user were to click an operation:
-        0 would continue as operand1 with operation performing on it
-    IF the user were to click another number while operation is setup and the equal sign is clicked:
+        Previous number would be set to the current number and current number would be set to nothing
+        Operation would join the previous number in the docked section of the display
+    IF the user were to click any of the operators or the equal sign when both numbers and the operator is set up:
         Run operate function with set operator and operands
 
 
 */
 
-/* 
-
-*/
 function readyOperation(input) {
     if (input.target.classList.contains('num')) {
-        inputs.push(parseInt(input.target.textContent));
+        if (current_num == 0 || current_num === null) { //Should change current_num whether current_num is the default of string 0 or null
+            current_num = input.target.textContent;
+        }
+        else {
+            current_num += input.target.textContent;
+        }
+        //Update staged display with current_num
     }
     else if (input.target.classList.contains('operator')) {
-        operator_input = input.target.id;
-    }
-    else if (input.target.id == 'operate') {
-        operate(operator_input, ...inputs);
+        if (prev_num && current_num) {
+            operate(operator_input, prev_num, current_num)
+            if (operator_input === 'operate') {
+                //Docked display shows expression 
+                //Staged display shows result
+            }
+            else {
+                //Docked display shows result concatenated with ` ${input.target.id}`
+                //Staged display shows result
+            }
+        }
+        else if (prev_num === null) {
+            prev_num = current_num;
+            current_num = null;
+            operator_input = input.target.id;
+            //Docked display would have prev_num concatenated with ` ${operator_input}`
+            //Staged display shows prev_num
+        }
+        else {
+            operator_input = input.target.id;
+            //Docked display would have prev_num concatenated with new ` ${operator_input}`
+        }
     }
 }
 
